afee98711bc32e7ec3585a4d70c0b7b1e08ec99b
Guilherme Oliveira <guilherme.carvalho@creditas.com.br>
2020-08-31 22:15:33 -0300

    Add conky dotfiles

---
+++ .conky/conky-startup.sh
@@ -0,0 +1,8 @@
+sleep 20s
+killall conky
+cd "/home/codus/.conky/CPUPanel"
+conky -c "/home/codus/.conky/CPUPanel/CPUPanel - 8 Core CPU" &
+cd "/home/codus/.conky/Conky Seamod"
+conky -c "/home/codus/.conky/Conky Seamod/conky_seamod" &
+cd "/home/codus/.conky/Green Apple Desktop"
+conky -c "/home/codus/.conky/Green Apple Desktop/Gotham" &
